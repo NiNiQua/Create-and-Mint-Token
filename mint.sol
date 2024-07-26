082d3213187b4f1f781ade26f79b456e222b7c24
Xzaniqua <92087430+NiNiQua@users.noreply.github.com>
2024-07-26 22:45:15 +0800

    Update mint.sol

--- mint.sol
+++ mint.sol
@@ -1,33 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract MyToken {
-  address public owner;
-  uint256 public totalSupply;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  mapping(address => uint256) public balances;
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {}
 
-  constructor() {
-      owner = msg.sender;
-      totalSupply = 0;
-  }
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
 
-  function mint(address _address, uint256 _amount) public {
-      require(msg.sender == owner, "Only the owner can mint tokens.");
-      require(_address == owner, "Tokens can only be minted to the owner.");
-      totalSupply += _amount;
-      balances[_address] += _amount;
-  }
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+    }
 
-  function burn(uint256 _amount) public {
-      require(balances[msg.sender] >= _amount, "Not enough balance.");
-      totalSupply -= _amount;
-      balances[msg.sender] -= _amount;
-  }
-
-  function transfer(address _to, uint256 _amount) public {
-      require(balances[msg.sender] >= _amount, "Not enough balance.");
-      balances[msg.sender] -= _amount;
-      balances[_to] += _amount;
-  }
+    function transfer(address _to, uint256 _amount) public override returns (bool) {
+        _transfer(msg.sender, _to, _amount);
+        return true; 
+    }
 }
